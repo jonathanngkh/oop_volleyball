1f9d13977b5403423f6134f35cd108df75377431
Jonathan Ng <jonngkh@hotmail.com>
2015-09-30 17:20:57 +0100

    More refactors and set up test for class methods on players for gender

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,7 +1,9 @@
 describe 'beachvolleyball' do
 
-  let(:team) {Team.new}
-  let(:player) { Player.new }
+  let(:team)          { Team.new      }
+  let(:player)        { Player.new    }
+  let(:male_player)   { Player.male   }
+  let(:female_player) { Player.female }
 
   it 'the team is not ready without players' do
     expect(team).not_to be_ready
@@ -14,6 +16,13 @@ describe 'beachvolleyball' do
 
   it 'the team is not ready when it has too many players' do
     11.times{team.add(player)}
+    expect(team).not_to be_ready
+  end
+
+  it 'the team is not ready when it has too few women' do
+    team.add female_player
+    7.times { team.add male_player }
+
     expect(team).not_to be_ready
   end
 end
