47cdc9ffe21730c4e4dfe283f4c419dea7d3b5b5
Jonathan Ng <jonngkh@hotmail.com>
2015-09-30 16:56:22 +0100

    triangulation team.ready test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,6 +1,14 @@
 describe 'beachvolleyball' do
+
   it 'the team is not ready without players' do
     team = Team.new
-    expect(team.ready?).to eq false
+    expect(team).not_to be_ready
   end
+
+  it 'the team is ready when it has enough players' do
+    team = Team.new
+    7.times{team.add(Player.new)}
+    expect(team).to be_ready
+  end
+
 end
